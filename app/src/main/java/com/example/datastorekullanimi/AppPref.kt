98c1b174e9a831364460e63580ee673e1760138d
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-01-30 13:01:01 +0300

    Apppref class and document create, read & delete function add project

--- app/src/main/java/com/example/datastorekullanimi/AppPref.kt
+++ app/src/main/java/com/example/datastorekullanimi/AppPref.kt
@@ -1,4 +1,56 @@
 package com.example.datastorekullanimi
 
-class AppPref {
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.*
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.first
+
+class AppPref(var context: Context) {
+    val Context.ds : DataStore<Preferences> by preferencesDataStore("bilgiler")
+
+    companion object {
+        val AD_KEY = stringPreferencesKey("AD")
+        val YAS_KEY = intPreferencesKey("YAS")
+        val BOY_KEY = doublePreferencesKey("BOY")
+        val BEKAR_KEY = booleanPreferencesKey("BEKAR")
+        val ARKADAS_LİSTE_KEY = stringSetPreferencesKey("ARKADAS_LISTE")
+    }
+
+    suspend fun kayitAd(ad: String) { // Suspend olması işlem zaman alacak bekle demek.
+        context.ds.edit {
+            it[AD_KEY] = ad
+        }
+    }
+
+    suspend fun okuAd(): String {
+        val p = context.ds.data.first()
+        return p[AD_KEY] ?: "isim yok"
+    }
+
+    suspend fun silAd() {
+        context.ds.edit {
+            it.remove(AD_KEY)
+        }
+    }
+    suspend fun kayitYas(yas: Int) { // Suspend olması işlem zaman alacak bekle demek.
+        context.ds.edit {
+            it[YAS_KEY] = yas
+        }
+    }
+
+    suspend fun okuYas(): Int {
+        val p = context.ds.data.first()
+        return p[YAS_KEY] ?: 0
+    }
+    suspend fun kayitBoy(boy: Double) { // Suspend olması işlem zaman alacak bekle demek.
+        context.ds.edit {
+            it[BOY_KEY] = boy
+        }
+    }
+
+    suspend fun okuBoy(): Double {
+        val p = context.ds.data.first()
+        return p[BOY_KEY] ?: 0.0
+    }
 }
--- app/src/main/java/com/example/datastorekullanimi/MainActivity.kt
+++ app/src/main/java/com/example/datastorekullanimi/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.datastorekullanimi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val ap = AppPref(this)
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            //ap.kayitAd("Ahmet")
+            ap.silAd()
+
+            val gelenad = ap.okuAd()
+            Log.e("Gelen Ad", gelenad)
+        }
     }
 }
